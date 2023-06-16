b72799e2de61448baac41479bcb79dc3c6df0bbb
Nathalia <63376794+nathaliaautran@users.noreply.github.com>
2023-06-16 13:35:01 -0300

    lampada.js

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -3,12 +3,12 @@ const turnOff = document.getElementById( 'turnOff' );
 const lamp = document.getElementById( 'lamp' );
 
 function isLampBroken() {
-    return lamp.src.indexOf('quebrada') > -1
+    return lamp.src.indexOf( 'quebrada' ) > -1
 }
 
 function lampOn() {
     if (!isLampBroken ()) {
-        lamp.src='./img/ligada.jpg';
+        lamp.src = './img/ligada.jpg';
     }
 }
 
@@ -19,11 +19,11 @@ function lampOff() {
 }
 
 function lampBroken() {
-    return src='./img/quebrada.jpg';
+    lamp.src = './img/quebrada.jpg';
 }
 
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff);
-lamp.addEventListener( 'dblclick', lampBroken );
+lamp.addEventListener('dblclick', lampBroken);
